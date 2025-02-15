953b895c227467761816afb8b00cd53382c3e591
harrrshith <harshith.kotian24@gmail.com>
2025-02-16 00:35:38 +0530

    add-basic-navigation
    navigate between screens

--- composeApp/src/commonMain/kotlin/com/harrrshith/moowe/ui/navigation/NavigationGraph.kt
+++ composeApp/src/commonMain/kotlin/com/harrrshith/moowe/ui/navigation/NavigationGraph.kt
@@ -1,11 +1,16 @@
 package com.harrrshith.moowe.ui.navigation
 
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import com.harrrshith.moowe.ImageCarousel
 import com.harrrshith.moowe.ui.onBoarding.OnBoardingRoute
+import com.harrrshith.moowe.width
 
 @Composable
 fun NavigationGraph(
@@ -13,13 +18,23 @@ fun NavigationGraph(
     navController: NavHostController
 ) {
     val startDestination = Destination.OnBoard
+    val navigate = remember { NavigationHelper(navController) }
     NavHost(
         modifier = modifier,
         navController = navController,
         startDestination = startDestination
     ){
         composable<Destination.OnBoard> {
-            OnBoardingRoute()
+            OnBoardingRoute(
+                navigateToHome = { navigate.navigateToHome() }
+            )
+        }
+
+        composable<Destination.Home.Discover> {
+            ImageCarousel(
+                screenWidth = width,
+                modifier = Modifier.fillMaxSize()
+            )
         }
     }
 }
